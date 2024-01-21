6517a69f29d156fa7800de5e9e07594c5e687a80
sofie <sophiess@seznam.cz>
2024-01-21 21:49:05 +0100

    - cors proxy used

--- src/components/rate-item/rate-item-styles.tsx
+++ src/components/rate-item/rate-item-styles.tsx
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const Item = styled.div`
   list-style-type: none;
-  line-height: 2;
+  line-height: 1.8;
 `;
--- src/shared/requests/rates.ts
+++ src/shared/requests/rates.ts
@@ -1,15 +1,15 @@
 const RATES_URL =
   'https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt';
+const CORS_PROXY_API_KEY = `${process.env.REACT_APP_CORS_PROXY_API_KEY}`;
 
 export const getRates = async (): Promise<string> => {
-  // const url = 'https://proxy.cors.sh/' + RATES_URL;
-  //
-  // const res = await fetch(url, {
-  //   headers: {
-  //     'x-cors-api-key': 'temp_ef4dca19859a421ad6eb433619309ee7',
-  //   },
-  // });
+  const url = 'https://proxy.cors.sh/' + RATES_URL;
 
-  const res = await fetch(RATES_URL);
+  const res = await fetch(url, {
+    headers: {
+      'x-cors-api-key': CORS_PROXY_API_KEY,
+    },
+  });
+
   return res.text();
 };
